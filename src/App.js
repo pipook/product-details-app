f2c5b8e4b80aed5529f3bbde647945d480e833ce
Daniel Chico <pipook@gmail.com>
2021-07-22 17:11:35 -0500

    Dandole estilos a la vista inicial, se agrega la barra de navegacion y una imagen inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className={classes.Topbar}>
+          <span></span>
+        </nav>
       </header>
+      <div className={classes.MainContainer}>
+        <div className={classes.ProductPreview}>
+          <img src="https://imgur.com/iOeUBV7.png" alt=""/>
+        </div>
+      </div>
     </div>
   );
 }
